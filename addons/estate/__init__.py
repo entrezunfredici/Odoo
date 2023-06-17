b8eb36cc43abab8a384917a1688fb3fe0820188c
superfred2468 <superfred2468@gmail.com>
2023-06-17 21:41:11 +0200

    First

---
+++ addons/estate/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#import sol_product_fields
+from . import models
+from . import report
+from . import controlleurs
+from . import data
+from . import static
+from . import views
---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "estate",
+    'version': '16.0',
+    'author': "Macabiau Frédéric",
+    'category': 'estate',
+    'description': """
+    Description text
+    """,
+    "depends": ["base"],  # dependencies
+    "data": [
+
+    ],
+    "application": True,  # This line says the module is an App, and not a module
+    "installable": True,
+    'license': 'LGPL-3',
+}
