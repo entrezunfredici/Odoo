4f5d732b08a6fd983df37060c67f6c09f22f97bc
superfred2468 <superfred2468@gmail.com>
2023-07-30 15:45:55 +0200

    add some actions

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 #Real Estate Odoo module created by Macabiau Frederic
 #Module database model
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
 
 class Property(models.Model):
     _name="estate.property"
@@ -11,7 +13,7 @@ class Property(models.Model):
     name=fields.Char("name", required=True) #required fields
     description=fields.Text("description")
     postcode=fields.Char("postcode")
-    date_availability=fields.Date("Available From", copy=False, default=lambda self: fields.Datetime.today())
+    date_availability=fields.Date("Available From", copy=False, default=lambda self: (fields.Datetime.today()))
     expected_price=fields.Float("price", required=True) #required fields
     selling_price=fields.Float("selling price", copy=False, readonly=True)
     bedrooms=fields.Integer("bedrooms", default=2)
@@ -37,7 +39,8 @@ class Property(models.Model):
             ('New','New'),
             ('Offer Received','Received'),
             ('Offer Accepted','Accepted'),
-            ('Sold and canceled','Sold')
+            ('Sold','Sold'),
+            ('Canceled','Canceled')
         ],
         default='New'
     )
@@ -46,3 +49,39 @@ class Property(models.Model):
     property_type_id=fields.Many2one('estate.property.type', string="property.type")
     property_tags_ids=fields.Many2many("estate.property.tags", string="property.tags")
     offer_ids=fields.One2many("estate.property.offer", "property_id", string="property offer")
+    #computed variable
+    total_area=fields.Float(compute="_compute_total_area")
+    best_price=fields.Float(compute="_compute_best_price")
+    #compute functions
+    @api.depends("living_area","garden_area")
+    def _compute_total_area(self):
+        for estate_property in self:
+            estate_property.total_area=estate_property.living_area+estate_property.garden_area
+    @api.depends("offer_ids")
+    def _compute_best_price(self):
+        for estate_property in self:
+            estate_property.best_price=max(estate_property.offer_ids.mapped("price")) if estate_property.offer_ids else 0.0
+    #onchange functions
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area=10
+            self.garden_orientation="N"
+        else:
+            self.garden_area=0
+            self.garden_orientation=False
+    #buttons functions
+    def estate_property_action_cancel(self):
+        for estate_property in self:
+            if(estate_property.state=="Sold"):
+                raise UserError('Sold property cannot be canceled')
+            else:
+                estate_property.state="Canceled"
+                return True
+    def estate_property_action_sold(self):
+        for estate_property in self:
+            if(estate_property.state=="Canceled"):
+                raise UserError('Canceled property cannot be sold')
+            else:
+                estate_property.state="Sold"
+                return True
--- addons/estate/models/estate_property_offer.py
+++ addons/estate/models/estate_property_offer.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #Real Estate Odoo module created by Macabiau Frederic
 #Module database model
-from odoo import fields, models
+from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
 
 class Property_offer(models.Model):
     _name="estate.property.offer"
@@ -11,8 +12,30 @@ class Property_offer(models.Model):
         string='status',
         selection=[
             ('Accepted','Accepted'),
-            ('Refuser','Refused')
+            ('Refused','Refused')
         ]
     )
     partner_id=fields.Many2one('res.partner', string='partner', required='true')
     property_id=fields.Many2one('estate.property', string='property', required='true')
+    validity=fields.Integer("validity", default=7)
+    #computed variables
+    #date_deadline=fields.Date(compute="_compute_create_date", inverse="_inverse_date_deadline")
+    #date_validity=fields.Date()
+    #compute functions
+    #@api.depends("")
+    #def _compute_date_deadline(self):
+    #    for offer in self:
+    #        offer.date_deadline=fields.Datetime.today()#+relativedelta(mounth=3)
+    #def _inverse_date_deadline(self):
+    #    for offer in self:
+    #        offer.date_validity=relativedelta(days=7)
+            #offer.date_validity=offer.date_deadline-fields.Datetime.today()
+    #button functions
+    def estate_property_offer_action_Accept(self):
+        for offer in self:
+            offer.status="Accepted"
+            offer.property_id.selling_price=offer.price
+            offer.property_id.state="Sold"
+    def estate_property_offer_action_Refuse(self):
+        for offer in self:
+            offer.status="Refused"
