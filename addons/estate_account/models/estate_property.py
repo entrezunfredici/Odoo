05c74148bd460d4c43dfc691b0360a004a5c63a4
superfred2468 <superfred2468@gmail.com>
2023-09-06 11:00:18 +0200

    interact with other modules

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -9,18 +9,18 @@ class estate_property(models.Model):
         """ When an invoice linked to a sales order selling registrations is
         paid confirm attendees. Attendees should indeed not be confirmed before
         full payment. """
-        price=fields.Integer("price")
         price=int(self.selling_price*1.06+100.00)
-        account_ids.account=self.partner_id
-        account_ids.move_type='out_invoyce',
+        name=self.partner_id
         self.env["account.move"].create(
             {
                 "name":"account.move",
                 "line_ids":[
-                    Command.create({
-                        "name": "self.name",
-                        "quantity": "1",
-                        "price_unit": price
+                    Command.create({ 
+                        "name": 'self.name',
+                        "quantity": '1',
+                        "price_unit": price,
+                        "partner_id": self.partner_id,
+                        "move_type": 'out_invoyce',
                     })
                 ],
             }
